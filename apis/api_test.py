39b345844585c0c429902f018768bef968d94bf8
Duo Tao <taod@carleton.edu>
2016-04-15 10:23:38 -0500

    Fixed the data-not-exist bug

--- apis/api_test.py
+++ apis/api_test.py
@@ -5,13 +5,14 @@
     all nearby restaurants once given a location and a distance from the
     location. After returning all the name of the restaurants, users can
     also query for the details of those retaurants.
-'''  
+'''
 
 import sys
 import argparse
 import json
 import urllib.request
-   
+
+
 def get_restaurants(location_la, location_lo, radius):
     '''
     Get the names of all restaurants within a distance from a location.
@@ -56,9 +57,11 @@ def get_details(place_id):
         return {}
     return json_from_server['result']
 
+
 def main(args):
     # search for restaurants
-    restaurant_list = get_restaurants(args.latitude, args.longitude, args.radius)
+    restaurant_list = get_restaurants(
+        args.latitude, args.longitude, args.radius)
     index = 1
 
     # list and print them
@@ -72,30 +75,55 @@ def main(args):
     # let the users get details of the restaurants
     while True:
         try:
-            selected_index = input('Please enter an index to get details (or \'quit\' to quit):')
+            selected_index = input(
+                'Please enter an index to get details (or \'quit\' to quit):')
             if selected_index == 'quit':
                 break
-            print ('---------- {0} ----------'.format(restaurant_list[int(selected_index) - 1]['name']))
+            print (
+                '---------- {0} ----------'.format(restaurant_list[int(selected_index) - 1]['name']))
 
             # query again to get the details
-            details = get_details(restaurant_list[int(selected_index) - 1]['place_id'])
-            print (details['formatted_address'])
-            print (details['formatted_phone_number'])
-            if details['opening_hours']['open_now']:
-                print ('Open')
+            details = get_details(
+                restaurant_list[int(selected_index) - 1]['place_id'])
+            if ('formatted_address' in details):
+                print (details['formatted_address'])
             else:
-                print ('Closed')
-            for time in details['opening_hours']['weekday_text']:
-                print (time)
-            print ("Price Level: {0}".format(details['price_level']))
-            print ("Rating: {0}".format(details['rating']))
-            print ("Find on Map: {0}".format(details['url']))
-            print ('Website: {0}'.format(details['website']))
-        except Exception:
+                print ("No address given")
+            if ('formatted_phone_number' in details):
+                print (details['formatted_phone_number'])
+            else:
+                print ("No phone number given")
+            if ("opening_hours" in details):
+                if details['opening_hours']['open_now']:
+                    print ('Open')
+                else:
+                    print ('Closed')
+                for time in details['opening_hours']['weekday_text']:
+                    print (time)
+                if ('price_level' in details):
+                    print ("Price Level: {0}".format(details['price_level']))
+                else:
+                    print("No Price Level Given")
+            else:
+                print ("No opening hours offered")
+            if ('rating' in details):
+                print ("Rating: {0}".format(details['rating']))
+            else:
+                print("No rating Given")
+            if ('url' in details):
+                print ("Find on Map: {0}".format(details['url']))
+            else:
+                print("No Map Url Given")
+            if ('website' in details):
+                print ('Website: {0}'.format(details['website']))
+            else:
+                print("No website Given")
+        except Exception as e:
             print ('Please enter a valid input')
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Get all restaurants near a location from Google Maps')
+    parser = argparse.ArgumentParser(
+        description='Get all restaurants near a location from Google Maps')
 
     parser.add_argument('latitude',
                         type=float,
@@ -107,7 +135,7 @@ if __name__ == '__main__':
                         metavar='longitude',
                         help='The longitude of where you start')
 
-    parser.add_argument('radius', 
+    parser.add_argument('radius',
                         metavar='radius',
                         type=float,
                         help='the radius of the area you want to search from the location')
