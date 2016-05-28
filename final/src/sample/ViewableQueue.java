1ab867c8703eb14c043fc4b5013c853106ab0ba5
Duo Tao <taod@carleton.edu>
2016-05-28 00:48:38 -0500

    change one line of the doc

--- final/src/sample/ViewableQueue.java
+++ final/src/sample/ViewableQueue.java
@@ -34,7 +34,7 @@ public class ViewableQueue implements ViewableDataStructure {
 
     /**
      * Returns visual representation of nodes of the data structure
-     * @return
+     * @return a list of Nodes with one Node for each entry
      */
     @Override
     public List<Node> getNodes() {
@@ -49,12 +49,27 @@ public class ViewableQueue implements ViewableDataStructure {
     public List<ControlWrapper> getControls() {
         List<ControlWrapper> list = new ArrayList<ControlWrapper>();
         TextField textField = new TextField();
-        Button button = new Button("Enqueue");
+        Button button = new Button("enqueue");
         EventHandler<ActionEvent> handler = (event) -> {
-            this.queue.add(Integer.parseInt(textField.getText()));
+            this.queue.offer(Integer.parseInt(textField.getText()));
         };
         ControlWrapper wrapper = new ControlWrapper(textField, button, handler);
-        list.add(wrapper);
+
+        TextField textField1 = new TextField();
+        Button button1 = new Button("dequeue");
+        EventHandler<ActionEvent> handler1 = (event) -> {
+            textField1.setText(Integer.toString(this.queue.poll()));
+        };
+        ControlWrapper wrapper1 = new ControlWrapper(textField1, button1, handler1);
+        list.add(wrapper1);
+
+        TextField textField2 = new TextField();
+        Button button2 = new Button("getFront");
+        EventHandler<ActionEvent> handler2 = (event) -> {
+            this.queue.peek();
+        };
+        ControlWrapper wrapper2 = new ControlWrapper(textField2, button2, handler2);
+        list.add(wrapper2);
         // Other operations go here...
         return list;
     }
