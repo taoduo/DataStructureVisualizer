4db81bdf79445cdfb240a52de7e08af82529bdf9
yijun wang <yijun wang>
2016-05-31 09:53:44 -0500

    Fixed a small error in comment.

--- final/src/sample/VisualizedStack.java
+++ final/src/sample/VisualizedStack.java
@@ -87,7 +87,7 @@ public class VisualizedStack extends VisualizedDataStructure {
         ControlWrapper controlWrapper2 = new ControlWrapper(textField2, button2, eventHandler2);
         list.add(controlWrapper2);
 
-        // add the pop operation
+        // add the peek operation
         Button button3 = new Button("Peek");
         TextField textField3 = new TextField();
         textField3.setDisable(true);
