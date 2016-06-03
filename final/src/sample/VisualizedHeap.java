75f6ef921c8e9a3cb1ed9b33a1f5608de6827b91
Duo Tao <taod@carleton.edu>
2016-06-03 14:45:22 -0500

    minor change, keep up to date

--- final/src/sample/VisualizedHeap.java
+++ final/src/sample/VisualizedHeap.java
@@ -1,13 +1,16 @@
 package sample;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.PriorityQueue;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-
-import java.util.*;
 
 
 /**
