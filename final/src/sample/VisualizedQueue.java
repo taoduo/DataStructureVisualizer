b56feed6ed8c3760517be6b79459fefd0e7b3ea2
ClaireTagoe <tagoec@carleton.edu>
2016-06-03 23:30:41 -0500

    Implemented Queue

--- final/src/sample/VisualizedQueue.java
+++ final/src/sample/VisualizedQueue.java
@@ -11,7 +11,12 @@ import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 
 /**
  * The model class of Queue in MVC
@@ -19,6 +24,11 @@ import javafx.scene.control.TextField;
  */
 public class VisualizedQueue extends VisualizedDataStructure {
     private Queue<Integer> queue;
+    private static final int X_ADJUSTMENT = 25;
+    private static final int Y_ADJUSTMENT = 15;
+    private static final int LINE_WIDTH = 5;
+    private static final int RADIUS = 25;
+
     /**
      * Empty queue constructor
      */
@@ -35,7 +45,11 @@ public class VisualizedQueue extends VisualizedDataStructure {
      */
     @Override
     public void randomize(int size, int min, int max) {
-
+        this.queue.clear();
+        for (int i = 0; i < size; i++) {
+            int rand = (int) (Math.random() * (max - min + 1) + min);
+            this.queue.offer(rand);
+        }
     }
 
     /**
@@ -45,12 +59,47 @@ public class VisualizedQueue extends VisualizedDataStructure {
     @Override
     public List<Node> getNodes() {
         List<Node> list = new ArrayList<>();
+        int count = 0;
         if (this.queue.isEmpty()) {
             return list;
         } else {
-            for (Iterator<Integer> itr = this.queue.iterator(); itr.hasNext(); itr.next()){
-            //Should do something with value in queue later but this loop prints it for now
-                System.out.println(itr);
+
+            for (Iterator<Integer> itr = this.queue.iterator(); itr.hasNext();) {
+
+                Circle circle = new Circle(RADIUS);
+                circle.setCenterY(controller.displayBoard.getHeight() / 2);
+                circle.setCenterX(RADIUS + 2 * count * RADIUS + (LINE_WIDTH * count));
+                circle.setFill(Color.LIGHTGREY);
+                if (count == 0) {
+                    circle.setFill(Color.LIGHTBLUE);
+                } else if (count == this.queue.size() - 1 ) {
+                    circle.setFill(Color.LIGHTCYAN);
+                }
+                circle.setStroke(Color.BLACK);
+                Line line = new Line();
+                line.setStartX(circle.getCenterX() + RADIUS);
+                line.setStartY(controller.displayBoard.getHeight() / 2);
+                line.setEndX(line.getStartX() + LINE_WIDTH);
+                line.setEndY(controller.displayBoard.getHeight() / 2);
+                Label label;
+                if (count == 0) {
+                    label = new Label(itr.next().toString() + " \nfront");
+                } else if (count == this.queue.size()-1){
+                    label = new Label(itr.next().toString() + " \nend");
+                } else {
+                    label = new Label(itr.next().toString());
+
+                }
+
+                label.setLayoutY(controller.displayBoard.getHeight() / 2 - Y_ADJUSTMENT);
+                label.setLayoutX(circle.getCenterX() + RADIUS/2-X_ADJUSTMENT);
+
+
+                list.add(circle);
+                list.add(label);
+                if (count != this.queue.size() -1) list.add(line);
+
+                count++;
             }
         }
         return list;
@@ -125,22 +174,42 @@ public class VisualizedQueue extends VisualizedDataStructure {
     }
 
     /**
-     * Serialize the stack into a string
+     * Serialize the queue into a string
      * @return the serialization output as a string
      */
     @Override
     public String serialize() {
         String result = "";
+        for (Integer i : this.queue) {
+            result = result + " " + i;
+        }
         return result;
     }
 
     /**
-     * Deserialize the string and get the queue back
+     * Deserialize the string to initialize the queue
+     * Returns null if the input is misformatted
+     * Format: numbers splitted by one or more spaces, tabs or new lines
+     * Number on the right is on the top of the stack
      * @param stringRepresentation the input string
-     * @return true if the input is valid, false if not
+     * @return if the input is viable
      */
     @Override
     public boolean deserialize(String stringRepresentation) {
-        return false;
+        if (stringRepresentation.isEmpty()) {
+            this.queue.clear();
+            return true;
+        }
+        String[] arr = stringRepresentation.split("\\s+");
+        Queue<Integer> tempQueue = new ArrayDeque<Integer>();
+        for (String str : arr) {
+            if (this.isInt(str)) {
+                tempQueue.offer(Integer.parseInt(str));
+            } else {
+                return false;
+            }
+        }
+        this.queue = tempQueue;
+        return true;
     }
 }
